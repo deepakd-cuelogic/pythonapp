fc04d2cf3c305afa0d0ec50221effc65e6149346
Deepak Dubey <deepakkumar.dubey@cuelogic.com>
2016-12-21 13:41:25 +0530

    Modified test cases.

--- helloworld/tests.py
+++ helloworld/tests.py
@@ -1,6 +1,9 @@
 from rebar.testing import flatten_to_dict
+import django.http
+import django.utils.unittest as unittest2
 from contacts import forms
-...
+
+
 class EditContactFormTests(TestCase):
 
     def test_mismatch_email_is_invalid(self):
@@ -23,4 +26,15 @@ class EditContactFormTests(TestCase):
         form_data['confirm_email'] = 'foo@example.com'
 
         bound_form = forms.ContactForm(data=form_data)
-        self.assert_(bound_form.is_valid())	
+        self.assert_(bound_form.is_valid())
+
+
+    class LocaleMiddlewareTests(unittest2.TestCase):
+        """
+        LocalMiddleWare test cases.
+        """
+        def test_request_not_processed(self):
+            middleware = LocaleMiddle()
+            response = django.http.HttpResponse()
+            middleware.process_response(none, response)
+            self.assertFalse(response.cookies)
